af38c4381b9b163c8da10dda50bd7cb2737ef789
Mahir33 <adam.marek.wrobel@gmail.com>
2023-08-21 14:10:50 +0200

    main component added

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+export default function Main() {
+  return (
+    <section>
+      <h2>Title</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero quae
+        natus, expedita a quo iste et officia sit, atque neque consequatur!
+        Harum, reiciendis! Minus incidunt blanditiis quod? Consectetur nemo
+        officia, quo recusandae quis veritatis aspernatur ex tenetur molestias
+        nisi delectus aliquam quae ipsum praesentium ullam temporibus odit
+        accusantium sit reprehenderit magnam. Maiores omnis placeat ut. Tenetur
+        veniam totam eos facilis maiores accusamus, quaerat quo quidem omnis
+        voluptatum hic ut dolorem tempora quam ullam quis, explicabo inventore?
+        Distinctio voluptatem consequatur ab error officia corporis culpa
+        debitis libero officiis, doloremque, provident, quidem quis expedita
+        dolorum a exercitationem necessitatibus porro vitae ut repudiandae?
+      </p>
+    </section>
+  );
+}
